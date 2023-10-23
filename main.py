03428c80b495a5079b667262073eb4a71bf5ad26
Sourcery AI <>
2023-10-23 23:25:01 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ from bitcoin import *
 from hashlib import sha256
 
 def try_word(word):
-  print('Palabra ' + word)
+  print(f'Palabra {word}')
   priv = sha256(word).hexdigest()
-  print('Private Key:' + priv)
+  print(f'Private Key:{priv}')
   pub = privtopub(priv)
-  print('Public Key:' + pub)
+  print(f'Public Key:{pub}')
   addr = pubtoaddr(pub)
-  print('Address:' + addr)
+  print(f'Address:{addr}')
   # h = history(addr)
   # print(h)
   h = unspent(addr)
-  print('Cantidad ' + str(len(h)))
-  if(len(h) > 0):
-    return True
-  return False
+  print(f'Cantidad {len(h)}')
+  return len(h) > 0
 
 with open('encontrados.txt', 'w') as output_file:
   with open('smalldict.txt') as f:
